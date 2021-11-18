e5a01068df10e8053510210e80416a1a3866161f
Alerapi2001 <alerapi2001@gmail.com>
2021-11-18 14:42:29 +0100

    3 files modified

---
+++ script.js
@@ -0,0 +1,16 @@
+function sortTable(){
+    var tbl = document.getElementById("caltbl").tBodies[0];
+    var store = [];
+    for(var i=tbl.rows.length-1, len=0; i>=len; i--){
+        var row = tbl.rows[i];
+        var sortnr = parseFloat(row.cells[1].textContent || row.cells[1].innerText);
+        if(!isNaN(sortnr)) store.push([sortnr, row]);
+    }
+    store.sort(function(x,y){
+        return x[0] - y[0];
+    });
+    for(var i=store.length-1, len=0; i>=len; i--){
+        tbl.appendChild(store[i][1]);
+    }
+    store = null;
+}
